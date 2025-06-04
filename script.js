54f5981ba79de62c5e0374e3957e09945dd07426
mdahmed4875 <140884439+mdahmed4875@users.noreply.github.com>
2025-06-04 08:07:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
-
+    let form=document.getElementById('form1');
+	let fname=form['fname'].value;
+	let lname=form['lname'].value;
+	let fulln=fname+" "+lname;
+	alert(fulln);
 }
